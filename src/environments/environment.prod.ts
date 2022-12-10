ec3979d318ae015e3c6a7ffa0f7f07edae825118
Jonisoc <jonathan3492@hotmail.com>
2022-12-09 22:36:08 -0700

    Fix build error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDNlHn7MeEteecUpFKhor68WU9swubGkWg",
+    authDomain: "dah2022-adjam-aarl.firebaseapp.com",
+    projectId: "dah2022-adjam-aarl",
+    storageBucket: "dah2022-adjam-aarl.appspot.com",
+    messagingSenderId: "29081965674",
+    appId: "1:29081965674:web:1a5a90b1dbf2fb88e52613"
+  }
 };
