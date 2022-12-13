cc630fd377a378056753cb7dd8fd178f8043f765
Riv10 <aaalrivaslu@ittepic.edu.mx>
2022-12-12 23:03:02 -0700

    Camara

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Camera, CameraResultType} from '@capacitor/camera';
 
 @Component({
   selector: 'app-tab4',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
+  picture: string;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  async takePicture(){
+      const image = await Camera.getPhoto({
+        quality: 100,
+        allowEditing : false,
+        resultType: CameraResultType.DataUrl
+      });
+
+      this.picture = image.dataUrl;
   }
 
 }
