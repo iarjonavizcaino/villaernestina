74d6832d0ff1498f01e2b7a3a305fb170338d16e
Israel Arjona Vizcaino <iarjona@ittepic.edu.mx>
2024-06-11 23:11:49 -0700

    Add colibrí V0.25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {TranslateService } from '@ngx-translate/core';
+//import { register } from 'swiper/angular/';
 
 @Component({
   selector: 'app-root',
--- src/app/colibri/colibri.module.ts
+++ src/app/colibri/colibri.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ColibriPageRoutingModule } from './colibri-routing.module';
 
 import { ColibriPage } from './colibri.page';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ColibriPageRoutingModule
+    ColibriPageRoutingModule,
+    SwiperModule
   ],
   declarations: [ColibriPage]
 })
